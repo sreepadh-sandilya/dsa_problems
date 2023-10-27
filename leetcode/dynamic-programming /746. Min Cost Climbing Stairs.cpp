11cadfa7e70b5aba7f14007dd10d89bf6f58f2a0
sreepadh-sandilya <103916024+sreepadh-sandilya@users.noreply.github.com>
2023-10-27 16:21:04 +0530

    Update 746. Min Cost Climbing Stairs.cpp

--- leetcode/dynamic-programming /746. Min Cost Climbing Stairs.cpp
+++ leetcode/dynamic-programming /746. Min Cost Climbing Stairs.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    int solve1(int n,vector<int>&cost)
+    {
+        vector<int>dp(n+1);
+        if(n==0 || n==1)
+        {
+            return dp[n];
+        }
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        
+
+        for(int i=2;i<n;i++)
+        {
+            dp[i]=min(dp[i-1],dp[i-2])+cost[i];
+        }
+        return min(dp[n-1],dp[n-2]);
+    }
     int solve(int n,vector<int>&cost,vector<int>&dp)
     {
         if(n==0 || n==1)
@@ -17,7 +34,8 @@ public:
         int n=cost.size();
         vector<int>dp(n+1,-1);
         
-        return min(solve(n-1,cost,dp),solve(n-2,cost,dp));
+        // return min(solve(n-1,cost,dp),solve(n-2,cost,dp));
+        return solve1(n,cost);
         
     }
 };
