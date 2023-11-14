cfa0dbedbabd39d4146219b7f47342500e7ca1ef
Sreepadh <103916024+sreepadh-sandilya@users.noreply.github.com>
2023-11-14 14:47:28 +0530

    Create 62. Unique Paths.cpp

---
+++ leetcode/dynamic-programming /62. Unique Paths.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int solve(vector<vector<int>>&dp,int m,int n,int i,int j)
+    {
+        if(i==m-1 && j==n-1)
+        {
+            return 1;
+        }
+        if(i>=m || j>=n)
+        {
+            return 0;
+        }
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        int ans=0;
+        ans=solve(dp,m,n,i+1,j) + solve(dp,m,n,i,j+1);
+        dp[i][j]=ans;
+        return ans;
+
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return solve(dp,m,n,0,0);
+        
+    }
+};
