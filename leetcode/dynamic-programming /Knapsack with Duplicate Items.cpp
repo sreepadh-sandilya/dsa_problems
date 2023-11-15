3651305e898d642d452e62f3f61d251861c353db
Sreepadh <103916024+sreepadh-sandilya@users.noreply.github.com>
2023-11-15 10:12:30 +0530

    Create Knapsack with Duplicate Items.cpp

---
+++ leetcode/dynamic-programming /Knapsack with Duplicate Items.cpp
@@ -0,0 +1,39 @@
+class Solution{
+public:
+    int solve(vector<vector<int>>&dp,int w,int wt[], int val[],int n,int i,int sum)
+        {
+            if(i>=n)
+            {
+                return 0;
+            }
+            if(sum>w)
+            {
+                return 0;
+            }
+            if(dp[sum][i]!=-1)
+            {
+                return dp[sum][i];
+            }
+            int ans=INT_MIN,take=0,nottake=0;
+            if(sum+wt[i]<=w){
+            take=val[i]+solve(dp,w,wt,val,n,i,sum+wt[i]);
+            }
+            nottake=solve(dp,w,wt,val,n,i+1,sum);
+            ans=max(take,nottake);
+            dp[sum][i]=ans;
+            return ans;
+            
+        }
+    int knapSack(int n, int w, int val[], int wt[])
+    {
+        // code here
+        int sum=0;
+       for(int i=0;i<n;i++)
+       {
+           sum+=wt[i];
+       }
+       
+       vector<vector<int>>dp(w+1,vector<int>(n,-1));
+       return solve(dp,w,wt,val,n,0,0);
+    }
+};
