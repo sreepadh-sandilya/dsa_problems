0ee88a0bb27678c3b9104bfce04db84b2c467ece
Sreepadh <103916024+sreepadh-sandilya@users.noreply.github.com>
2023-11-23 20:52:54 +0530

    Update Longest Bitonic subsequence.cpp

--- leetcode/dynamic-programming /Longest Bitonic subsequence.cpp
+++ leetcode/dynamic-programming /Longest Bitonic subsequence.cpp
@@ -43,3 +43,42 @@ class Solution{
 	}
    
 };
+
+// bottom up approach:
+class Solution{
+	public:
+	int LongestBitonicSequence(vector<int>nums)
+	{
+	    // code here
+	    int n=nums.size();
+	    vector<int>dp1(n,1);
+	    vector<int>dp2(n,1);
+	    for(int i=1;i<n;i++)
+	    {
+	        for(int j=0;j<i;j++)
+	        {
+	            if(nums[i]>nums[j])
+	            {
+	                dp1[i]=max(dp1[i],1+dp1[j]);
+	            }
+	        }
+	    }
+	    
+	    for(int i=n-2;i>=0;i--)
+	    {
+	        for(int j=n-1;j>i;j--)
+	        {
+	            if(nums[i]>nums[j])
+	            {
+	                dp2[i]=max(dp2[i],1+dp2[j]);
+	            }
+	        }
+	    }
+	    int k=-1;
+	    for(int i=0;i<n;i++)
+	    {
+	        k=max(k,dp1[i]+dp2[i]-1);
+	    }
+	    return k;
+	}
+};
