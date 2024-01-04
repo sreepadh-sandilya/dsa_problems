97e95700c8fc4d58ebb781e142a101058550ad34
Sreepadh <103916024+sreepadh-sandilya@users.noreply.github.com>
2024-01-05 00:10:43 +0530

    Create 300. Longest Increasing Subsequence_binary_search.cpp

---
+++ leetcode/dynamic-programming /300. Longest Increasing Subsequence_binary_search.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int>temp;
+        int n=nums.size();
+        temp.push_back(nums[0]);
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i]>temp.back())
+            {
+                temp.push_back(nums[i]);
+            }
+            else{
+                    int ind=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+                    temp[ind]=nums[i];
+            }
+
+        }
+
+        return temp.size();
+        
+    }
+};
