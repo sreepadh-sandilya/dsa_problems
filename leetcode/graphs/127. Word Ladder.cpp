abf0f176472025ebc066ad8af346d4fe09b52230
Sreepadh <103916024+sreepadh-sandilya@users.noreply.github.com>
2024-02-02 19:03:37 +0530

    Update 127. Word Ladder.cpp

--- leetcode/graphs/127. Word Ladder.cpp
+++ leetcode/graphs/127. Word Ladder.cpp
@@ -43,3 +43,47 @@ public:
         return 0;
     }
 };
+
+
+// optimized:
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        queue<string>q;
+        q.push(beginWord);
+        int n=wordList.size(),ans=0;
+        vector<int>vis(n,-1);
+        unordered_set<string>st(wordList.begin(),wordList.end());
+        st.erase(beginWord);
+        while(!q.empty())
+        {
+            int siz=q.size();
+            for(int k=0;k<siz;k++){
+                string s=q.front();
+                if(s==endWord)
+                {
+                    return ans+1;
+                }
+                q.pop();
+                for(int i=0;i<s.size();i++){
+                    string o=s;
+                    for(char ch='a';ch<='z';ch++)
+                    {
+                        o[i]=ch;
+                        if(st.find(o)!=st.end())
+                        {
+                            st.erase(o);
+                            q.push(o);
+                        }
+
+                    }
+                }
+                
+            }
+            ans++;
+
+        }
+
+        return 0;
+    }
+};
