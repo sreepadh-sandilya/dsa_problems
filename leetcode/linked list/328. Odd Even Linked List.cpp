d5f90169ba8a3a683c0fe4863a67fa02c29b6ae7
Sreepadh <103916024+sreepadh-sandilya@users.noreply.github.com>
2024-02-13 15:34:00 +0530

    Create 328. Odd Even Linked List.cpp

---
+++ leetcode/linked list/328. Odd Even Linked List.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head==NULL)
+        {
+            return NULL;
+        }
+        ListNode* odd=head;
+        ListNode* even=head->next;
+        ListNode* o=head;
+        ListNode* e=head->next;
+        while(odd!=NULL && even!=NULL && odd->next!=NULL && even->next!=NULL)
+        {
+            odd->next=odd->next->next;
+            even->next=even->next->next;
+            odd=odd->next;
+            even=even->next;
+        }
+        if(odd==NULL)
+        {
+            odd=e;
+        }
+        else
+        {
+            odd->next=e;
+        }
+        return o;
+
+    }
+};
