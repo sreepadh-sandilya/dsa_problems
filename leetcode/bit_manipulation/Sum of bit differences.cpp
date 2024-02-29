e8eeef841b44339057131ec2b4c27d122e4d02e2
Sreepadh <103916024+sreepadh-sandilya@users.noreply.github.com>
2024-02-29 13:53:00 +0530

    Create Sum of bit differences.cpp

---
+++ leetcode/bit_manipulation/Sum of bit differences.cpp
@@ -0,0 +1,23 @@
+class Solution{
+public:
+	
+	long long sumBitDifferences(int arr[], int n) {
+	    // code here
+	    int long long ans=0;
+	    for(int i=0;i<32;i++)
+	    {
+	        long long count=0;
+	        for(int j=0;j<n;j++)
+	        {
+	            if(arr[j] & (1<<i))
+	            {
+	                count++;
+	            }
+	            
+	        }
+	        ans+=count*(n-count)*2;
+	    }
+	    
+	    return ans;
+	}
+};
