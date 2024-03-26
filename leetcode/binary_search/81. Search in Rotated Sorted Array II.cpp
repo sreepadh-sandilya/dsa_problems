55c532a2f8a9b4a231433cab98dfeb390a1b6eb8
Sreepadh <103916024+sreepadh-sandilya@users.noreply.github.com>
2024-03-26 14:06:20 +0530

    Create 81. Search in Rotated Sorted Array II.cpp

---
+++ leetcode/binary_search/81. Search in Rotated Sorted Array II.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+bool solve(vector<int>& nums, int target,int i,int j)
+    {
+        while(i<=j)
+        {
+            int m=(i+j)/2;
+            if(nums[m]==target)
+            {
+                return true;
+            }
+             
+            if(nums[i]==nums[m] && nums[j]==nums[m])
+            {
+                i++;
+                j--;
+                continue;
+            }
+
+
+
+            if(nums[i]<=nums[m])
+            {
+                if(nums[i]<=target && target<nums[m])
+                {
+                     j=m-1;
+                }
+                else
+                {
+                    i=m+1;
+                }
+            }
+            else
+            {
+                if(nums[m]<target && nums[j]>=target)
+                {
+                    i=m+1;
+                }
+                else
+                {
+                    j=m-1;
+                }
+            }
+        }
+        return false;
+    }
+    bool search(vector<int>& nums, int target) {
+        return solve(nums,target,0,nums.size()-1);
+    }
+};
