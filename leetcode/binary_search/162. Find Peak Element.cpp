66aa3877a188f04ef442b6e3f4deb1b412f74eb5
Sreepadh <103916024+sreepadh-sandilya@users.noreply.github.com>
2024-03-26 17:25:48 +0530

    Create 162. Find Peak Element.cpp

---
+++ leetcode/binary_search/162. Find Peak Element.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)
+        {
+            return 0;
+        }
+        if(nums[0]>nums[1])
+        {
+            return 0;
+        }
+        if(nums[n-1]>nums[n-2])
+        {
+            return n-1;
+        }
+        int i=1,j=n-2;
+        while(i<=j)
+        {
+            int m=(i+j)/2;
+            if(nums[m]>nums[m-1] && nums[m]>nums[m+1])
+            {
+                return m;
+            }
+            if(nums[m]>nums[m-1])
+            {
+                i=m+1;
+            }
+            else
+            {
+              j=m-1;
+            }
+        }
+        return -1;
+    }
+};
